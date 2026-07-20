471f232842fb32532a235b62b661c0e453a36973
Gladson Paul <g24.gladson.paul@gnkhalsa.edu.in>
2026-07-20 18:49:27 +0530

    Bad use case

--- Day10/P2.py
+++ Day10/P2.py
@@ -1,1 +1,14 @@
-# Threading Module: provides high-level interface for worjing with three
+# Threading Module: provides high-level interface for worjing with threads. 
+
+
+import threading
+import time
+
+def thread_info():
+    current_thread = threading.current_thread()
+    print(f"Thread Name:{current_thread.name}")
+    print(f"Thred ID:{current_thread.native_id}")
+    print(f"Is Daemon:{current_thread.daemon}")
+
+print("Main thread")
+thread_info
